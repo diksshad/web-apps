459da9244bb9ac8d40bfc599fcd960e407d08689
dikssha dinesh <dikssha.dinesh@kellogg.northwestern.edu>
2023-05-05 19:26:29 +0000

    commit1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+  resources "tacos"
 end
+
+#rails server
+#/tacos to the url
+#/ cntrl c gets you out of rails and into the linux command line
+#/ index.html.erb
+# 
